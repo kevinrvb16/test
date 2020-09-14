3d67948271b3b79219f13ee60a8ff5dd653ab721
kevin Rafael velez Bernal <kevinrvb16@github.com>
2020-09-14 16:06:49 -0300

    pagina pessoal adicionada

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+//for(let i = 0; i<1; i++){
+  //var conteudo= prompt("O que você aprendeu hoje?");
+  //var p = document.createElement("p")
+//}
+function criaLista(hobbie){
+  hobbie = document.getElementById("paragrafo");
+  document.createElement("p");
+return hobbie;
+
+}
+
+
+//var listaObj = document.getElementById("hobbie");
